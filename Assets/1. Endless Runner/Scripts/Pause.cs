2d2404fc8e2993d1f1920e1df3265c2c1f6b6cac
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-31 12:49:11 -0300

    menu pause

--- Assets/1. Endless Runner/Scripts/Pause.cs
+++ Assets/1. Endless Runner/Scripts/Pause.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject painelPause;
+    private bool jogoPausado = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (jogoPausado)
+                ContinuarJogo();
+            else
+                PausarJogo();
+        }
+    }
+
+    public void PausarJogo()
+    {
+        painelPause.SetActive(true);
+        Time.timeScale = 0f; // pausa o tempo
+        jogoPausado = true;
+    }
+
+    public void ContinuarJogo()
+    {
+        painelPause.SetActive(false);
+        Time.timeScale = 1f; // volta o tempo
+        jogoPausado = false;
+    }
+
+    public void ReiniciarJogo()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void SairDoJogo()
+    {
+        Debug.Log("Saindo do jogo...");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
     }
 }
